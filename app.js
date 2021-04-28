ae7744a41b69854e19037ea4c89f02c499d1a076
neryad <dayern.gomez@hotmail.com>
2021-04-28 17:28:40 -0400

    no puppeter

--- app.js
+++ app.js
@@ -129,4 +129,4 @@ const sendMessage = (to, message) => {
 };
 
 fs.existsSync(SESSION_FILE) ? withSession() : withOutSession();
-puppeteer.launch({ args: ['--no-sandbox'] });
+// puppeteer.launch({ args: ['--no-sandbox'] });
